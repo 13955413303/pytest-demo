2bff612ec16b3c824d4b24b9ec8dcb3adc9fcbde
zyang <zyang1209@outlook.com>
2021-07-31 10:41:26 +0800

    first commit

--- testcase/test_api.py
+++ testcase/test_api.py
@@ -12,14 +12,14 @@ class TestApi:
     # @pytest.mark.parametrize('name,age', [['aaa', 12], ['bbb', 18]])
     # def test_02(self, name, age):
     #     print(name, age)
-    def test_01(self):
-        url = r'http://127.0.0.1/api/mgr/customers?action=list_customer'
-        method= 'GET'
-        params = {
-            'action' : 'list_customer'
-        }
-        resp = requests.request(url=url,method=method,params=params)
-        print(resp.text)
+    # def test_01(self):
+    #     url = r'http://127.0.0.1/api/mgr/customers?action=list_customer'
+    #     method= 'GET'
+    #     params = {
+    #         'action' : 'list_customer'
+    #     }
+    #     resp = requests.request(url=url,method=method,params=params)
+    #     print(resp.text)
     @pytest.mark.parametrize('args',YamlUtil('test_api.yaml').read_yaml())
     def test_02(self,args):
         print(args)
